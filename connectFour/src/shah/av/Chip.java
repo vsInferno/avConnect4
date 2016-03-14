f92816c04cd980c0547acfa83045189bbec1529b
Vraj Shah <vrajshah842@gmail.com>
2016-03-14 18:52:30 -0400

    Further Initiation

--- connectFour/src/shah/av/Chip.java
+++ connectFour/src/shah/av/Chip.java
@@ -5,10 +5,24 @@ import java.awt.Color;
 public class Chip {
 
 	private static Color chipColor;
+	private static int chipDiameter;
 	
-	public Chip(Color color) {
+	public Chip(Color color, int chipDiameter) {
 		
 		this.chipColor = color;
+		this.chipDiameter = chipDiameter;
+		
+	}
+	
+	public void setColor(Color color) {
+		
+		this.chipColor = color;
+		
+	}
+	
+	public void setSize(int size) {
+		
+		this.chipDiameter = size;
 		
 	}
 	
--- connectFour/src/shah/av/ConnectFourPanel.java
+++ connectFour/src/shah/av/ConnectFourPanel.java
@@ -6,12 +6,23 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 
 public class ConnectFourPanel extends JPanel {
+	
+	Chip redChip;
+	Chip blueChip;
 
 	public ConnectFourPanel() {
 
 		setPreferredSize(new Dimension(600, 500));
+		setBackground(Color.YELLOW);
 
-		setLayout(new GridLayout(8, 7));
+		setLayout(new GridLayout(6, 7));
+		
+		//redChip.setSize(80);
+		//blueChip.setSize(80);
+		
+		//redChip.setColor(Color.RED);
+		//blueChip.setColor(Color.BLUE);
+
 		
 	}
 
